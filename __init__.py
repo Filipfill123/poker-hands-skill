0e22ffc0fadf893abee77e82b1bd0f909fc41618
Filip Polak <FilipPolak@seznam.cz>
2021-01-25 22:54:13 +0100

    First version

--- __init__.py
+++ __init__.py
@@ -7,7 +7,37 @@ class PokerHands(MycroftSkill):
 
     @intent_file_handler('hands.poker.intent')
     def handle_hands_poker(self, message):
-        self.speak_dialog('hands.poker')
+        hand_combination = message.data.get('combination')
+        if hand_combination == 'pair':
+            result = "two cards of the same rank"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == 'two pair':
+            result = "two different pairs"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == ('three of a kind' or "trips" or "set"):
+            result = "three cards of the same rank"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == "straight":
+            result = "five cards in a sequence, but not of the same suit"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == 'flush':
+            result = "any five cards of the same suit, but not in a sequence"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == ('full house' or "boat"):
+            result = "three of a kind with a pair"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == ('four of a kind' or "quads"):
+            result = "all four cards of the same rank"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == 'straight flush':
+            result = "five cards in a sequence, all in the same suit"
+            self.speak_dialog('hands.poker', data={"result": result})
+        elif hand_combination == ('royal flush' or "royal"):
+            result = "ace, king, queen, jack, ten, all the same suit"
+            self.speak_dialog('hands.poker', data={"result": result})
+        else:
+            self.speak_dialog('hands.poker.wrong')
+        
 
 
 def create_skill():
