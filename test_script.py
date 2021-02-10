8eda193be318cd3200efe7dc7362557e1c6bf771
Filip Polak <FilipPolak@seznam.cz>
2021-02-10 20:04:20 +0100

    Basic confirmation added, deleted old version

--- test_script.py
+++ test_script.py
@@ -1,6 +1,1 @@
 # test .py script for trial and error bullshit -> should be the library (in the future)
-STATE_REPRESENTATION_2 = {'user': "skill_name", 'task': "in_progress",'agent': {"first_card":["ace","confirmed"],"second_card":["king","not_confirmed"]}}
-#STATE_REPRESENTATION_2 = { 'first' : ["prdel", "prdel"],
-                            #"second": ["hovno", "hovno"]}
-STATE_REPRESENTATION_2['agent']['first_card'] = ["kunda","prdel"]
-print(STATE_REPRESENTATION_2['agent']['first_card'])                                                                           
