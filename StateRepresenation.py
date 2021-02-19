700f8937749a761d688ee8e901d5696c189f9e47
Filip Polak <FilipPolak@seznam.cz>
2021-02-19 21:49:50 +0100

    not working

--- StateRepresenation.py
+++ StateRepresenation.py
@@ -11,45 +11,172 @@
 #### 5) DELETE state representation
 
 class StateRepresentation:
-    def __init__(self, skill_name, no_of_slates, compulsory_slates, possible_outcomes):
-        # possible_outcomes = ["no_pair", "in_progress", "pair"]
-        task_state = possible_outcomes[1] # -> only three outcomes - no_pair, in_progress, pair (for simplicity as -1 0 1, hence 0 1 2)
-        state_representation = {'user': skill_name,'agent': {},'task': task_state}
-        for i in range(no_of_slates):
-            state_representation["agent"][f"card_{i+1}"] = [None,None,"compulsory"]
+    def __init__(self, skill_name, no_of_slates, is_confidence_score):
+        """
+        Initialization of STATE REPRESENTATION
+
+        Args: user's intent, number of slates (probably should be dynamic somehow), true if using confidence score/false if no confidence score
+        """
+        state_representation = {'user': skill_name,'agent': {},'task': "in_progress"}
+        if is_confidence_score:
+            for i in range(no_of_slates):
+                state_representation["agent"][f"slot_{i+1}"] = [[None, None],None] # [[ace,0.9], "not_confirmed"]
+        else:
+            for i in range(no_of_slates):
+                state_representation["agent"][f"slot_{i+1}"] = [None,None] # [ace, "not_confirmed"]
         
         self.no_of_slates = no_of_slates
-        self.STATE_REPRESENTATION = state_representation
+        self.STATE_REPRESENTATION = state_representation # INITIALIZATION
 
-    def get_card(self, which_card):
-        card = self.STATE_REPRESENTATION["agent"][f"card_{which_card}"][0]
-        return card
+
+    """
+    Part 1) no confidence score
+    """
+    def get_slot_value(self, which_slot):
+        """
+        For getting value of a specific slot = to be used if there are no unconsistencies, thus no confidence score
+
+        Args: number of slot to get
+        
+        Returns: value of a specific slot
+        """
+        slot_value = self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"][0]
+        return slot_value
     
-    def get_all_cards(self):
-        cards = list()
+
+    def get_all_slots_value(self): 
+        """
+        For getting value of all slots = to be used if there are no unconsistencies, thus no confidence score
+        
+        Returns: array of values for all slots ([ace, king]) 
+        """
+        slots_value = list()
         for i in range(self.no_of_slates):
-            cards.append(self.STATE_REPRESENTATION["agent"][f"card_{i+1}"][0])
-        return cards
+            slots_value.append(self.STATE_REPRESENTATION["agent"][f"slot_{i+1}"][0])
+        return slots_value
 
-    def set_card_not_cofirmed(self, card_value, which_card):
-        self.STATE_REPRESENTATION["agent"][f"card_{which_card}"] = [card_value,"not_confirmed", "compulsory"]
 
-    def set_card_cofirmed(self, which_card):
-        self.STATE_REPRESENTATION["agent"][f"card_{which_card}"][1] = "confirmed"
+    def set_slot_value_not_cofirmed(self, slot_value, which_slot):
+        """
+        For setting an unconfirmed value to a slot = to be used if there are no unconsistencies, thus no confidence score
 
-    def set_card_value_confirmed(self, card_value, which_card):
-        self.STATE_REPRESENTATION["agent"][f"card_{which_card}"] = [card_value,"confirmed", "compulsory"]
+        Args: value to set, number of slot
+        
+        """
+        self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"] = [slot_value,"not_confirmed"]
 
-    def set_outcome_pair(self):
-        self.STATE_REPRESENTATION["agent"]["task"] = "pair"
-    
-    def set_outcome_no_pair(self):
-        self.STATE_REPRESENTATION["agent"]["task"] = "no_pair"
+
+    def set_slot_cofirmed(self, which_slot):
+        """
+        For "switching"/setting a slot to a confirmed state  = to be used if there are no unconsistencies, thus no confidence score
+
+        Args: which slot to confirm
+        """
+        self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"][1] = "confirmed"
     
     def delete_state_representation(self):
+        """
+        Deletes all of agent's memory - primarely for debugging purposes
+        """
         for i in range(self.no_of_slates):
-            self.STATE_REPRESENTATION["agent"][f"card_{i+1}"] = [None, None, "compulsory"]
-    # def set_both_cards_not_confirmed(self, card_values): -> probably unnecessary
+            self.STATE_REPRESENTATION["agent"][f"slot_{i+1}"] = [None, None]
+
+
+    """
+    Part 2) with confidence score
+    """
+
+    def get_slot_value_with_confidence(self, which_slot):
+        """
+        For getting value of a specific slot and its confidence score = to be used if there are unconsistencies, thus with confidence score
+
+        Args: number of slot to get
+        
+        Returns: value of a specific slot and its confidence
+        """
+        slot_value = self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"][0]
+        return slot_value
+
+    def get_all_slot_values(self, which_slot):
+        """
+        For getting all values of one slot and their confidence scores = to be used if there are unconsistencies, thus with confidence score
+
+        Args: number of slot to get
+
+        Returns: array of arrays [[ace,0.9],[king,0.3],[two,0.05]] = [[value_1, confidence_score_1],[value_2, confidence_score_2],...]
+
+        """
+        slot_values = list()
+        for i in range(len(self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"][0])):
+            slot_values.append(self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"][0][i])
+        return slot_values
+
+    def set_slot_value_not_cofirmed_with_confidence(self, slot_value, confidence_score, which_slot):
+        """
+        For setting an unconfirmed value and its confidence to a slot = to be used if there are unconsistencies, thus with confidence score
+
+        Args: value, confidence score, number of slot  
+        
+        """
+        self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"] = [[slot_value,confidence_score],"not_confirmed"]
+
+
+    def set_slot_cofirmed_with_confidence(self, which_slot): # function without confidence could also be used
+        """
+        For "switching"/setting a slot to a confirmed state  = to be used if there are unconsistencies, thus with confidence score
+
+        Args: which slot to confirm
+        """
+        self.STATE_REPRESENTATION["agent"][f"slot_{which_slot}"][1] = "confirmed"
+    
+    def delete_state_representation_with_confidence(self):
+        """
+        Deletes all of agent's memory - primarely for debugging purposes
+        """
+        for i in range(self.no_of_slates):
+            self.STATE_REPRESENTATION["agent"][f"slot_{i+1}"] = [[None, None], None]
+
+
+    """
+    Part 3) confidence score does not matter
+    """
+    def get_task_state(self):
+        """
+        For getting state of task
+
+        Returns: state of task
+        """
+        task_state = self.STATE_REPRESENTATION["task"]
+
+        return task_state
+
+    def get_user_state(self):
+        """
+        For getting state of user
+
+        Returns: state of user
+        """
+        user_state = self.STATE_REPRESENTATION["user"]
+        
+        return user_state
+
+    def set_user_state(self, user_state):
+        """
+        For setting a user state
+
+        Args: the user state to be set
+        """
+        self.STATE_REPRESENTATION["user"] = user_state
+
+    def set_task_state(self, task_state):
+        """
+        For setting a task state
+
+        Args: the task state to be set
+        """
+        self.STATE_REPRESENTATION["task"] = task_state
+
+    
 
     
 
--- __init__.py
+++ __init__.py
@@ -7,9 +7,7 @@ class PokerHands(MycroftSkill):
     def __init__(self):
         self.skill_name = "poker_hands"
         self.no_of_slates = 2 # 2 cards
-        self.compulsory_slates = 2 # both cards need to be known
-        self.possible_outcomes = = ["no_pair", "in_progress", "pair"]
-        self.STATE_REPRESENTATION = StateRepresentation(self.skill_name, self.no_of_slates, self.compulsory_slates, self.possible_outcomes)
+        self.STATE_REPRESENTATION = StateRepresentation(self.skill_name, self.no_of_slates, False)
         # STATE_REPRESENTATION should be connection of user's intention + agent's state + task being "worked on"
         # states of task - ["in_progress" - in progress (the task is being worked on), "pair" - it's a pair, "no_pair" - it's not a pair]
         # states of slots - first value - card (if None, not understood or not answered yet), second value = [None - empty, "not_confirmed" - not confirmed, "confirmed" - confirmed, "inconsistent" - more than one value for slot]
@@ -27,125 +25,129 @@ class PokerHands(MycroftSkill):
             self.speak_dialog('hands.poker.all.wrong')
         
         elif (first_card not in self.cards) and (second_card in self.cards): # first card was misunderstood
-            self.STATE_REPRESENTATION.set_card_not_cofirmed(second_card,2)
+            self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(second_card,2)
             self.speak_dialog('hands.poker.first.wrong', data={"second_card": second_card})
         
         elif (first_card in self.cards) and (second_card not in self.cards): # second card was misunderstood
-            self.STATE_REPRESENTATION.set_card_not_cofirmed(first_card,1)
+            self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(first_card,1)
             self.speak_dialog('hands.poker.second.wrong', data={"first_card": first_card})
         
         else:
-            self.STATE_REPRESENTATION['agent']['first_card'] = [first_card, "not_confirmed"]
-            self.STATE_REPRESENTATION['agent']['second_card'] = [second_card,"not_confirmed"]
+            self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(first_card, 1)
+            self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(second_card, 2)
             self.speak_dialog('confirm.cards',data={'first_card':first_card, 'second_card':second_card})
             
     @intent_file_handler('first.card.hands.poker.intent')
     def handle_first_card_hands_poker(self, message):
         first_card = message.data.get('first_card')
-        if self.STATE_REPRESENTATION.get_card(1) is not None:
-            result = f"I already know the first card. It is {self.STATE_REPRESENTATION.get_card(1)}. Please, what is the second card?"
+        if self.STATE_REPRESENTATION.get_slot_value(1) is not None:
+            result = f"I already know the first card. It is {self.STATE_REPRESENTATION.get_slot_value(1)}. Please, what is the second card?"
             self.speak_dialog('hands.poker', data={"result": result})
-        elif self.STATE_REPRESENTATION.get_card(2) is None and first_card in self.cards:    
-            self.STATE_REPRESENTATION.set_card_not_cofirmed(first_card, 1)
+        elif self.STATE_REPRESENTATION.get_slot_value(2) is None and first_card in self.cards:    
+            self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(first_card, 1)
             result = f"The first card is {first_card}. What is the second card?"
             self.speak_dialog('hands.poker', data={"result": result})   
         else:
             if first_card not in self.cards:
                 self.speak_dialog('notunderstood.hands.poker',data={'which_card': "first card"}) 
             else:
-                self.STATE_REPRESENTATION.set_card_not_cofirmed(first_card, 1)
-                self.speak_dialog('confirm.cards',data={'first_card':first_card, 'second_card':self.STATE_REPRESENTATION.get_card(2)})
+                self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(first_card, 1)
+                self.speak_dialog('confirm.cards',data={'first_card':first_card, 'second_card':self.STATE_REPRESENTATION.get_slot_value(2)})
 
     @intent_file_handler('second.card.hands.poker.intent')
     def handle_second_card_hands_poker(self, message):
         second_card = message.data.get('second_card')
-        if self.STATE_REPRESENTATION.get_card(2) is not None:
-            result = f"I already know the second card. It is {self.STATE_REPRESENTATION.get_card(2)}. Please, what is the first card?"
+        if self.STATE_REPRESENTATION.get_slot_value(2) is not None:
+            result = f"I already know the second card. It is {self.STATE_REPRESENTATION.get_slot_value(2)}. Please, what is the first card?"
             self.speak_dialog('hands.poker', data={"result": result})
-        elif self.STATE_REPRESENTATION.get_card(1) and second_card in self.cards:
-            self.STATE_REPRESENTATION.set_card_not_cofirmed(second_card, 2)
+        elif self.STATE_REPRESENTATION.get_slot_value(1) and second_card in self.cards:
+            self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(second_card, 2)
             result = f"The second card is {second_card}. What is the first card?"
             self.speak_dialog('hands.poker', data={"result": result})
         else:    
             if second_card not in self.cards:
                 self.speak_dialog('notunderstood.hands.poker',data={'which_card': "second card"}) 
             else:
-                self.STATE_REPRESENTATION.set_card_not_cofirmed(second_card, 2)
+                self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(second_card, 2)
                 
-                self.speak_dialog('confirm.cards',data={'first_card':self.STATE_REPRESENTATION.get_card(1), 'second_card':second_card})
+                self.speak_dialog('confirm.cards',data={'first_card':self.STATE_REPRESENTATION.get_slot_value(1), 'second_card':second_card})
 
 
     @intent_file_handler('confirm.both.cards.intent')
     def handle_confirm_both_cards_intent(self, message):
-        self.STATE_REPRESENTATION.set_card_cofirmed(1)
-        self.STATE_REPRESENTATION.set_card_cofirmed(2)
+        self.STATE_REPRESENTATION.set_slot_cofirmed(1)
+        self.STATE_REPRESENTATION.set_slot_cofirmed(2)
+        time = datetime.datetime.now()
 
-    
-        if self.STATE_REPRESENTATION.get_card(1) == self.STATE_REPRESENTATION.get_card(2):
-            result = f"it is a pair of {self.STATE_REPRESENTATION.get_card(1)}s"
+        if self.STATE_REPRESENTATION.get_slot_value(1) == self.STATE_REPRESENTATION.get_slot_value(2):
+            result = f"it is a pair of {self.STATE_REPRESENTATION.get_slot_value(1)}s"
             self.STATE_REPRESENTATION.delete_state_representation()
-            self.STATE_REPRESENTATION.set_outcome_pair()
-            time = datetime.datetime.now()
+            self.STATE_REPRESENTATION.set_task_state("pair")
+            
             with open("logs/log.txt", "w") as logging:
-                write.logging(f"{time}: {self.STATE_REPRESENTATION.get_card(1)} + {self.STATE_REPRESENTATION.get_card(2)} = pair"
+                logging.write(f"{time}: {self.STATE_REPRESENTATION.get_slot_value(1)} + {self.STATE_REPRESENTATION.get_slot_value(2)} = {self.STATE_REPRESENTATION.get_task_state()}")
             
             self.speak_dialog('hands.poker', data={"result": result})
         else:
-            result = f"{self.STATE_REPRESENTATION.get_card(1)} and {self.STATE_REPRESENTATION.get_card(2)} is not a pair"
+            result = f"{self.STATE_REPRESENTATION.get_slot_value(1)} and {self.STATE_REPRESENTATION.get_slot_value(2)} is not a pair"
 
             self.STATE_REPRESENTATION.delete_state_representation()
-            self.STATE_REPRESENTATION.set_outcome_no_pair()
+            self.STATE_REPRESENTATION.set_task_state("no_pair")
             with open("logs/log.txt", "w") as logging:
-                write.logging(f"{time}: {self.STATE_REPRESENTATION.get_card(1)} + {self.STATE_REPRESENTATION.get_card(2)} = no_pair"
+                logging.write(f"{time}: {self.STATE_REPRESENTATION.get_slot_value(1)} + {self.STATE_REPRESENTATION.get_slot_value(2)} = {self.STATE_REPRESENTATION.get_task_state()}")
 
             self.speak_dialog('hands.poker', data={"result": result})
     
     @intent_file_handler('confirm.first.card.intent')
     def handle_confirm_first_card_intent(self, message):
-        self.STATE_REPRESENTATION.set_card_cofirmed(1)
+        self.STATE_REPRESENTATION.set_slot_cofirmed(1)
         second_card = message.data.get('second_card')
-        self.STATE_REPRESENTATION.set_card_value_confirmed(second_card, 2)
+        self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(second_card, 2)
+        self.STATE_REPRESENTATION.set_slot_cofirmed(2)
+        time = datetime.datetime.now()
 
-        if self.STATE_REPRESENTATION.get_card(1) == self.STATE_REPRESENTATION.get_card(2):
-            result = f"it is a pair of {self.STATE_REPRESENTATION.get_card(1)}s"
+        if self.STATE_REPRESENTATION.get_slot_value(1) == self.STATE_REPRESENTATION.get_slot_value(2):
+            result = f"it is a pair of {self.STATE_REPRESENTATION.get_slot_value(1)}s"
 
             self.STATE_REPRESENTATION.delete_state_representation()
-            self.STATE_REPRESENTATION.set_outcome_pair()
+            self.STATE_REPRESENTATION.set_task_state("pair")
             with open("logs/log.txt", "w") as logging:
-                write.logging(f"{time}: {self.STATE_REPRESENTATION.get_card(1)} + {self.STATE_REPRESENTATION.get_card(2)} = pair")
+                logging.write(f"{time}: {self.STATE_REPRESENTATION.get_slot_value(1)} + {self.STATE_REPRESENTATION.get_slot_value(2)} = {self.STATE_REPRESENTATION.get_task_state()}")
 
             self.speak_dialog('hands.poker', data={"result": result})
         else:
-            result = f"{self.STATE_REPRESENTATION.get_card(1)} and {self.STATE_REPRESENTATION.get_card(2)} is not a pair"
+            result = f"{self.STATE_REPRESENTATION.get_slot_value(1)} and {self.STATE_REPRESENTATION.get_slot_value(2)} is not a pair"
 
             self.STATE_REPRESENTATION.delete_state_representation()
-            self.STATE_REPRESENTATION.set_outcome_no_pair()
+            self.STATE_REPRESENTATION.set_task_state("no_pair")
             with open("logs/log.txt", "w") as logging:
-                write.logging(f"{time}: {self.STATE_REPRESENTATION.get_card(1)} + {self.STATE_REPRESENTATION.get_card(2)} = no_pair")
+                logging.write(f"{time}: {self.STATE_REPRESENTATION.get_slot_value(1)} + {self.STATE_REPRESENTATION.get_slot_value(2)} = {self.STATE_REPRESENTATION.get_task_state()}")
             self.speak_dialog('hands.poker', data={"result": result})
 
     @intent_file_handler('confirm.second.card.intent')
     def handle_confirm_second_card_intent(self, message):
-        self.STATE_REPRESENTATION.set_card_cofirmed(2)
+        self.STATE_REPRESENTATION.set_slot_cofirmed(2)
         first_card = message.data.get('first_card')
-        self.STATE_REPRESENTATION.set_card_value_confirmed(first_card, 1)
+        self.STATE_REPRESENTATION.set_slot_value_not_cofirmed(first_card, 1)
+        self.STATE_REPRESENTATION.set_slot_cofirmed(1)
+        time = datetime.datetime.now()
 
-        if self.STATE_REPRESENTATION.get_card(1) == self.STATE_REPRESENTATION.get_card(2):
-            result = f"it is a pair of {self.STATE_REPRESENTATION.get_card(1)}s"
+        if self.STATE_REPRESENTATION.get_slot_value(1) == self.STATE_REPRESENTATION.get_slot_value(2):
+            result = f"it is a pair of {self.STATE_REPRESENTATION.get_slot_value(1)}s"
 
             self.STATE_REPRESENTATION.delete_state_representation()
-            self.STATE_REPRESENTATION.set_outcome_pair()
+            self.STATE_REPRESENTATION.set_task_state("pair")
             with open("logs/log.txt", "w") as logging:
-                write.logging(f"{time}: {self.STATE_REPRESENTATION.get_card(1)} + {self.STATE_REPRESENTATION.get_card(2)} = pair")
+                logging.write(f"{time}: {self.STATE_REPRESENTATION.get_slot_value(1)} + {self.STATE_REPRESENTATION.get_slot_value(2)} = {self.STATE_REPRESENTATION.get_task_state()}")
             
             self.speak_dialog('hands.poker', data={"result": result})
         else:
-            result = f"{self.STATE_REPRESENTATION.get_card(1)} and {self.STATE_REPRESENTATION.get_card(2)} is not a pair"
+            result = f"{self.STATE_REPRESENTATION.get_slot_value(1)} and {self.STATE_REPRESENTATION.get_slot_value(2)} is not a pair"
 
             self.STATE_REPRESENTATION.delete_state_representation()
-            self.STATE_REPRESENTATION.set_outcome_no_pair()
+            self.STATE_REPRESENTATION.set_task_state("no_pair")
             with open("logs/logg.txt", "w") as logging:
-                write.logging(f"{time}: {self.STATE_REPRESENTATION.get_card(1)} + {self.STATE_REPRESENTATION.get_card(2)} = no_pair")
+                logging.write(f"{time}: {self.STATE_REPRESENTATION.get_slot_value(1)} + {self.STATE_REPRESENTATION.get_slot_value(2)} = {self.STATE_REPRESENTATION.get_task_state()}")
             
             self.speak_dialog('hands.poker', data={"result": result})
     
@@ -157,24 +159,24 @@ class PokerHands(MycroftSkill):
     
     @intent_file_handler('kill.intent')
     def handle_kill(self, message):
-        if self.STATE_REPRESENTATION.get_card(1) is None and self.STATE_REPRESENTATION.get_card(2) is None:
+        if self.STATE_REPRESENTATION.get_slot_value(1) is None and self.STATE_REPRESENTATION.get_slot_value(2) is None:
             result = f"Wait a second, your highness. I looked really hard but my state representation seems to be empty already. Please, don't kill me"
-        elif self.STATE_REPRESENTATION.get_card(1) is None and self.STATE_REPRESENTATION.get_card(2) is not None:
-            result = f"The first card was empty, the second card was {self.STATE_REPRESENTATION.get_card(2)}"
-        elif self.STATE_REPRESENTATION.get_card(1) is not None and self.STATE_REPRESENTATION.get_card(2) is None:    
-            result = f"The first card was {self.STATE_REPRESENTATION.get_card(1)}, the second card was empty"
+        elif self.STATE_REPRESENTATION.get_slot_value(1) is None and self.STATE_REPRESENTATION.get_slot_value(2) is not None:
+            result = f"The first card was empty, the second card was {self.STATE_REPRESENTATION.get_slot_value(2)}"
+        elif self.STATE_REPRESENTATION.get_slot_value(1) is not None and self.STATE_REPRESENTATION.get_slot_value(2) is None:    
+            result = f"The first card was {self.STATE_REPRESENTATION.get_slot_value(1)}, the second card was empty"
         self.STATE_REPRESENTATION.delete_state_representation()
 
         self.speak_dialog('kill', data={"result": result})
 
     @intent_file_handler('show.intent')
     def handle_show(self, message):
-        if self.STATE_REPRESENTATION.get_card(1) is None and self.STATE_REPRESENTATION.get_card(2) is None:
+        if self.STATE_REPRESENTATION.get_slot_value(1) is None and self.STATE_REPRESENTATION.get_slot_value(2) is None:
             result = f"The state representation is empty, my lord"
-        elif self.STATE_REPRESENTATION.get_card(1) is None and self.STATE_REPRESENTATION.get_card(2) is not None:
-            result = f"The first card is empty, the second card is {self.STATE_REPRESENTATION.get_card(2)}"
-        elif self.STATE_REPRESENTATION.get_card(1) is not None and self.STATE_REPRESENTATION.get_card(2) is None:    
-            result = f"The first card is {self.STATE_REPRESENTATION.get_card(1)}, the second card is empty"
+        elif self.STATE_REPRESENTATION.get_slot_value(1) is None and self.STATE_REPRESENTATION.get_slot_value(2) is not None:
+            result = f"The first card is empty, the second card is {self.STATE_REPRESENTATION.get_slot_value(2)}"
+        elif self.STATE_REPRESENTATION.get_slot_value(1) is not None and self.STATE_REPRESENTATION.get_slot_value(2) is None:    
+            result = f"The first card is {self.STATE_REPRESENTATION.get_slot_value(1)}, the second card is empty"
 
         self.speak_dialog('show', data={"result": result})
 
