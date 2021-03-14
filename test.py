5b44cbf2f6f1f0a4bb580e1539a99ec5465453c6
Filip Polak <FilipPolak@seznam.cz>
2021-03-14 13:56:02 +0100

    more testing on __setattr__, __getattr__

--- test.py
+++ test.py
@@ -2,21 +2,46 @@ from pyrsistent import pvector, v
 
 class Slot():
     
-    def __init__(self):
-        self.value = Value()
-        self.state = "unconfirmed"
+    #def __init__(self):
+       # self.Value = Value()
+       # self.State = None
   
     def __getattribute__(self, name):
-        return super(Slot, self).__getattribute__(name)    # Gets called when the item is not found via __getattribute__
+        return super().__getattribute__(name)
 
     def __getattr__(self, name):
-        return super(Slot, self).__setattr__(name, 'nothing')
+        return super().__setattr__(name, None)
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
+
+    def is_valid(self, value):
+        if value == "ace":
+            return True
+        else:
+            return False
 
 class Value():
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.value = None
         self.confidence = 1.0
+        self.state = None
+        if len(args) != 0:
+            self.value = args[0]
+        if len(kwargs) != 0:
+            #print(kwargs)
+            self.confidence = kwargs['confidence']
+        
+
+    def __getattribute__(self, name):
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        return super().__setattr__(name, None)
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
 
 
 class State():
@@ -27,6 +52,12 @@ class State():
         self.Task = Slot()
         self.History = History()
 
+    def __getattribute__(self, name):
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        return super().__setattr__(name, None)
+
 
 class History():
 
@@ -34,11 +65,20 @@ class History():
         self.history = v()
 
 
-
 if __name__ == "__main__":
 
-    test = Slot()
-    print(test.prdel)
-    print(test.prdel)
+    # test = State()
+    # Value = Value("ace", confidence=0.8, confirmed=False)
+    # if Value.value == "ace" and Value.confidence == 0.8:
+    #     print("lol")
+
+    test_slot = Slot()
+    test_slot.first_card = Value("ace", confidence=0.8, confirmed=False)
+    test_slot.second_card = Value("ace", confidence=0.9, confirmed=False)
+
+    if test_slot.first_card.value == test_slot.second_card.value:
+        print("jop")
+    #test.User.Value = Value(2)
+    #print(test.User.Value.value)
     #test.value.value = "ace"
     #print(test.value.confidence)
