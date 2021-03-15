430d311b3eba5e398837f86ecea05a88705228b3
Filip Polak <FilipPolak@seznam.cz>
2021-03-15 20:50:56 +0100

    sorting done

--- test.py
+++ test.py
@@ -13,7 +13,11 @@ class Slot():
         return super().__setattr__(name, None)
 
     def __setattr__(self, name, value):
-        self.__dict__[name] = value
+        if self.value is None:
+            self.__dict__[name] = [value]
+        else:
+            self.__dict__[name].append(value)
+            self.__dict__[name].sort(key=lambda x: x.confidence, reverse=True)
 
     def is_valid(self, value):
         if value == "ace":
@@ -30,7 +34,6 @@ class Value():
         if len(args) != 0:
             self.value = args[0]
         if len(kwargs) != 0:
-            #print(kwargs)
             self.confidence = kwargs['confidence']
         
 
@@ -42,22 +45,25 @@ class Value():
 
     def __setattr__(self, name, value):
         self.__dict__[name] = value
+            
 
 
 class State():
 
-    def __init__(self):
-        self.Agent = Slot()
-        self.User = Slot()
-        self.Task = Slot()
-        self.History = History()
+    # def __init__(self):
+    #     self.Agent = Slot()
+    #     self.User = Slot()
+    #     self.Task = Slot()
+    #     self.History = History()
 
     def __getattribute__(self, name):
         return super().__getattribute__(name)
 
     def __getattr__(self, name):
         return super().__setattr__(name, None)
-
+        
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
 
 class History():
 
@@ -71,13 +77,15 @@ if __name__ == "__main__":
     # Value = Value("ace", confidence=0.8, confirmed=False)
     # if Value.value == "ace" and Value.confidence == 0.8:
     #     print("lol")
-
-    test_slot = Slot()
-    test_slot.first_card = Value("ace", confidence=0.8, confirmed=False)
-    test_slot.second_card = Value("ace", confidence=0.9, confirmed=False)
-
-    if test_slot.first_card.value == test_slot.second_card.value:
-        print("jop")
+    test_state = State()
+    first_card = Slot()
+    second_card = Slot()
+    first_card.value = Value("ace", confidence=0.1, confirmed=False)
+    first_card.value = Value("king", confidence=0.9, confirmed=False)
+    second_card.value = Value("king", confidence=0.9, confirmed=False)
+    test_state.first_card = first_card
+    test_state.second_card = second_card
+    print(first_card.value[0].confidence, first_card.value[1].confidence)
     #test.User.Value = Value(2)
     #print(test.User.Value.value)
     #test.value.value = "ace"
