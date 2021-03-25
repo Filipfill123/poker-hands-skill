d981d54d55f91b352935e6697f5745e494798db5
Filip Polak <FilipPolak@seznam.cz>
2021-03-25 14:38:43 +0100

    no biggie

--- test.py
+++ test.py
@@ -12,17 +12,18 @@ class Slot():
         return super().__setattr__(name, None)
 
     def __setattr__(self, name, value):
-        #print(value)
+        
         if name == 'value':
-            if self.is_valid(value.value):
+            print(value.value_confidence)
+            if self.is_valid(value.value_confidence['value']):
                 if self.value is None:
                     self.__dict__[name] = [value]
                     self.__dict__['state'] = 'unconfirmed'
                 else:
                     self.__dict__[name].append(value)
-                    self.__dict__[name].sort(key=lambda x: x.confidence, reverse=True)
+                    self.__dict__[name].sort(key=lambda x: x.value_confidence['confidence'], reverse=True)
                     self.__dict__['state'] = 'inconsistent'
-                self.__dict__['first_value'] = self.__dict__[name][0].value
+                self.__dict__['first_value'] = self.__dict__[name][0].value_confidence['value']
             else:
                 print("value not valid")
         else:
@@ -39,14 +40,11 @@ class Slot():
 
 class Value():
 
-    def __init__(self, *args, **kwargs):
-        self.value = None
-        self.confidence = 1.0
-        if len(args) != 0:
-            self.value = args[0]
-        if len(kwargs) != 0:
-            if 'confidence' in kwargs:
-                self.confidence = kwargs['confidence']
+    def __init__(self, **kwargs):
+        if 'value' in kwargs and 'confidence' in kwargs:
+            self.value_confidence = m(value=kwargs['value'], confidence=kwargs['confidence'])
+        else:
+            self.state = m(value=kwargs['state'])
 
     def __getattribute__(self, name):
         return super().__getattribute__(name)
@@ -69,7 +67,10 @@ class State():
         self.History = History()
 
     def __getattribute__(self, name):
+        #print(self.__dict__[name])
         return super().__getattribute__(name)
+        
+        #return self.__dict__[name].first_value
 
     def __getattr__(self, name):
         return super().__setattr__(name, None)
@@ -90,32 +91,27 @@ class History():
     def __init__(self):
         self.history = v()
 
-    def __getattribute__(self, name):
-        return super().__getattribute__(name)
+    # def __getattribute__(self, name):
+    #     return super().__getattribute__(name)
 
-    def __getattr__(self, name):
-        return super().__setattr__(name, None)
-
-    def __setattr__(self, name, value):
-        self.__dict__[name] = value
-
+    # def __getattr__(self, name):
+    #     return super().__setattr__(name, None)
             
-    def __delattr__(self, name):
-         del self.__dict__[name]
+    # def __delattr__(self, name):
+    #      del self.__dict__[name]
 
 if __name__ == "__main__":
-
     
     test_state = State()
-    History = History()
-    #print(History.history)
     #user = Slot()
     #task = Slot()
     first_card = Slot()
     #second_card = Slot()
-    first_card.value = Value("king", confidence=0.1)
-    first_card.value = Value("ace", confidence=0.9)
-    print(first_card.first_value)
+    print(first_card.state)
+    first_card.value = Value(value="king", confidence=0.1)
+    first_card.value = Value(value="ace", confidence=0.9)
+    test_state.first_card = first_card
+    print(test_state.first_card)
     #print(test_state.History.history)
     #print(test_state.prdel)
     #print(test_state.prdel)
